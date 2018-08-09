cb824fc3fa65ae822cc8c543398441ee50904f6e
Dylan Stackhouse <Stackhouse.Dylan@gmail.com>
2018-08-09 13:32:19 -0700

    update package/manifest config and project structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,45 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
-
-var path = require("path");
-var config = {
-  entry: ["./index.tsx"],
+const config = {
+  entry: {
+    main: './index.tsx',
+    'content': './src/content.ts',
+    'background': './src/background.ts',
+  },
   output: {
-    path: path.join(__dirname, '/build'),
-    filename: "bundle.js"
+    path: path.join(__dirname, '/dist'),
+    filename: '[name].bundle.js'
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: ['.ts', '.tsx', '.js']
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: "ts-loader",
+        loader: 'ts-loader',
         exclude: /node_modules/
       },{
         test: /\.scss$/,
-        loader: ["style-loader", "css-loader", "sass-loader"]
+        loader: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
-  }
+  },
+  devServer: {
+    port: 8000,
+    contentBase: path.resolve(__dirname, '.'),
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ]
 };
 
 module.exports = config;
