e8344f3e44aca3388852b360fcbd572da5a7c315
Dylan Stackhouse <Stackhouse.Dylan@gmail.com>
2018-09-24 21:38:37 -0700

    redirect user to options page on banned domains

--- src/content.ts
+++ src/content.ts
@@ -7,3 +7,23 @@ chrome.runtime.onMessage.addListener(
     }
   }
 );
+
+(function checkUrl() {
+  console.log('doing stuff here');
+  chrome.storage.local.get(['domains'], result => {
+    if(!result) {
+      return;
+    }
+
+    let localDomain = window.location.host.toLowerCase();
+    localDomain = localDomain.substring(0, 4) == 'www.' ? localDomain.substring(4): localDomain;
+
+    result.domains.forEach(domain => {
+      if (domain.domain.includes(localDomain)) {
+        let optionsUrl = chrome.extension.getURL('index.html'); 
+
+        window.location.href = optionsUrl;
+      }
+    });
+  });
+})()
