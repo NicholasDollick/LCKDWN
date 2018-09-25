db34e99be84731130e5546fa9875dced5175a647
Dylan Stackhouse <Stackhouse.Dylan@gmail.com>
2018-09-24 21:09:05 -0700

    open options page when extension icon is clicked

--- src/background.ts
+++ src/background.ts
@@ -11,3 +11,25 @@ chrome.runtime.onMessage.addListener(
     // recieve
   }
 );
+
+function openOrFocusOptionsPage() {
+   var optionsUrl = chrome.extension.getURL('options.html'); 
+   chrome.tabs.query({}, function(extensionTabs) {
+      var found = false;
+      for (var i=0; i < extensionTabs.length; i++) {
+         if (optionsUrl == extensionTabs[i].url) {
+            found = true;
+            console.log("tab id: " + extensionTabs[i].id);
+            chrome.tabs.update(extensionTabs[i].id, {"selected": true});
+         }
+      }
+      if (found == false) {
+          chrome.tabs.create({url: "index.html"});
+      }
+   });
+}
+ 
+// Called when the user clicks on the browser action icon.
+chrome.browserAction.onClicked.addListener(function(tab) {
+   openOrFocusOptionsPage();
+});
